c21ccbb2f4210e84ef8b13bc2bb662c1be566e57
JAVA_ACTVITY_IT202NIS <donald222java@gmail.com>
2026-04-18 22:17:39 +0800

    Add files via upload

---
+++ JAVA_FINALTERM_ACTIVITY3/JAVA_FINALTERM_ACTIVITY3/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace JAVA_FINALTERM_ACTIVITY3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.budtxt = new System.Windows.Forms.TextBox();
+            this.costtxt = new System.Windows.Forms.TextBox();
+            this.Importtxt = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbUrgency = new System.Windows.Forms.ComboBox();
+            this.cmbRisk = new System.Windows.Forms.ComboBox();
+            this.lblAffordabilityResult = new System.Windows.Forms.Label();
+            this.lblPriorityResult = new System.Windows.Forms.Label();
+            this.lblRiskResult = new System.Windows.Forms.Label();
+            this.lblDecisionResult = new System.Windows.Forms.Label();
+            this.lblExplanationResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Budget";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estimated Cost";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Urgency Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Importance Level";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 196);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Risk Level";
+            // 
+            // budtxt
+            // 
+            this.budtxt.Location = new System.Drawing.Point(195, 23);
+            this.budtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.budtxt.Name = "budtxt";
+            this.budtxt.Size = new System.Drawing.Size(252, 22);
+            this.budtxt.TabIndex = 5;
+            // 
+            // costtxt
+            // 
+            this.costtxt.Location = new System.Drawing.Point(195, 74);
+            this.costtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.costtxt.Name = "costtxt";
+            this.costtxt.Size = new System.Drawing.Size(252, 22);
+            this.costtxt.TabIndex = 6;
+            // 
+            // Importtxt
+            // 
+            this.Importtxt.Location = new System.Drawing.Point(195, 162);
+            this.Importtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Importtxt.Name = "Importtxt";
+            this.Importtxt.Size = new System.Drawing.Size(252, 22);
+            this.Importtxt.TabIndex = 8;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(549, 255);
+            this.Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(180, 48);
+            this.Button1.TabIndex = 11;
+            this.Button1.Text = "Evaluate";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(777, 255);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 48);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbUrgency
+            // 
+            this.cmbUrgency.FormattingEnabled = true;
+            this.cmbUrgency.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+            this.cmbUrgency.Location = new System.Drawing.Point(195, 117);
+            this.cmbUrgency.Name = "cmbUrgency";
+            this.cmbUrgency.Size = new System.Drawing.Size(252, 24);
+            this.cmbUrgency.TabIndex = 13;
+            // 
+            // cmbRisk
+            // 
+            this.cmbRisk.FormattingEnabled = true;
+            this.cmbRisk.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+            this.cmbRisk.Location = new System.Drawing.Point(195, 197);
+            this.cmbRisk.Name = "cmbRisk";
+            this.cmbRisk.Size = new System.Drawing.Size(252, 24);
+            this.cmbRisk.TabIndex = 14;
+            // 
+            // lblAffordabilityResult
+            // 
+            this.lblAffordabilityResult.AutoSize = true;
+            this.lblAffordabilityResult.Location = new System.Drawing.Point(619, 26);
+            this.lblAffordabilityResult.Name = "lblAffordabilityResult";
+            this.lblAffordabilityResult.Size = new System.Drawing.Size(0, 16);
+            this.lblAffordabilityResult.TabIndex = 15;
+            // 
+            // lblPriorityResult
+            // 
+            this.lblPriorityResult.AutoSize = true;
+            this.lblPriorityResult.Location = new System.Drawing.Point(619, 74);
+            this.lblPriorityResult.Name = "lblPriorityResult";
+            this.lblPriorityResult.Size = new System.Drawing.Size(0, 16);
+            this.lblPriorityResult.TabIndex = 16;
+            // 
+            // lblRiskResult
+            // 
+            this.lblRiskResult.AutoSize = true;
+            this.lblRiskResult.Location = new System.Drawing.Point(619, 121);
+            this.lblRiskResult.Name = "lblRiskResult";
+            this.lblRiskResult.Size = new System.Drawing.Size(0, 16);
+            this.lblRiskResult.TabIndex = 17;
+            // 
+            // lblDecisionResult
+            // 
+            this.lblDecisionResult.AutoSize = true;
+            this.lblDecisionResult.Location = new System.Drawing.Point(619, 169);
+            this.lblDecisionResult.Name = "lblDecisionResult";
+            this.lblDecisionResult.Size = new System.Drawing.Size(0, 16);
+            this.lblDecisionResult.TabIndex = 18;
+            // 
+            // lblExplanationResult
+            // 
+            this.lblExplanationResult.AutoSize = true;
+            this.lblExplanationResult.Location = new System.Drawing.Point(619, 205);
+            this.lblExplanationResult.Name = "lblExplanationResult";
+            this.lblExplanationResult.Size = new System.Drawing.Size(0, 16);
+            this.lblExplanationResult.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 319);
+            this.Controls.Add(this.lblExplanationResult);
+            this.Controls.Add(this.lblDecisionResult);
+            this.Controls.Add(this.lblRiskResult);
+            this.Controls.Add(this.lblPriorityResult);
+            this.Controls.Add(this.lblAffordabilityResult);
+            this.Controls.Add(this.cmbRisk);
+            this.Controls.Add(this.cmbUrgency);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Importtxt);
+            this.Controls.Add(this.costtxt);
+            this.Controls.Add(this.budtxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Life Decision Support System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox budtxt;
+        private System.Windows.Forms.TextBox costtxt;
+        private System.Windows.Forms.TextBox Importtxt;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbUrgency;
+        private System.Windows.Forms.ComboBox cmbRisk;
+        private System.Windows.Forms.Label lblAffordabilityResult;
+        private System.Windows.Forms.Label lblPriorityResult;
+        private System.Windows.Forms.Label lblRiskResult;
+        private System.Windows.Forms.Label lblDecisionResult;
+        private System.Windows.Forms.Label lblExplanationResult;
+    }
+}
+
---
+++ JAVA_FINALTERM_ACTIVITY3/JAVA_FINALTERM_ACTIVITY3/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAVA_FINALTERM_ACTIVITY3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsEmpty(string input)
+        {
+            return string.IsNullOrWhiteSpace(input);
+        }
+
+        private bool IsNumeric(string input)
+        {
+            return double.TryParse(input, out _);
+        }
+
+        private bool ValidateAllInputs()
+        {
+            if (IsEmpty(budtxt.Text))
+            { ShowError("Budget cannot be empty.", budtxt); return false; }
+
+            if (IsEmpty(costtxt.Text))
+            { ShowError("Estimated Cost cannot be empty.", costtxt); return false; }
+
+            if (IsEmpty(Importtxt.Text))
+            { ShowError("Importance Level cannot be empty.", Importtxt); return false; }
+
+            if (cmbUrgency.SelectedIndex == -1)
+            { ShowError("Please select an Urgency Level.", cmbUrgency); return false; }
+
+            if (cmbRisk.SelectedIndex == -1)
+            { ShowError("Please select a Risk Level.", cmbRisk); return false; }
+
+            if (!IsNumeric(budtxt.Text))
+            { ShowError("Budget must be a valid number.", budtxt); return false; }
+
+            if (!IsNumeric(costtxt.Text))
+            { ShowError("Estimated Cost must be a valid number.", costtxt); return false; }
+
+            if (!IsNumeric(Importtxt.Text))
+            { ShowError("Importance Level must be a number (1–10).", Importtxt); return false; }
+
+            int importance = int.Parse(Importtxt.Text);
+            if (importance < 1 || importance > 10)
+            { ShowError("Importance Level must be between 1 and 10.", Importtxt); return false; }
+
+            if (double.Parse(budtxt.Text) <= 0)
+            { ShowError("Budget must be greater than zero.", budtxt); return false; }
+
+            if (double.Parse(costtxt.Text) <= 0)
+            { ShowError("Estimated Cost must be greater than zero.", costtxt); return false; }
+
+            return true;
+        }
+
+        private void ShowError(string message, Control focusTarget)
+        {
+            MessageBox.Show(message, "Validation Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            focusTarget.Focus();
+        }
+
+        private string CheckAffordability(double budget, double cost)
+        {
+            double ratio = cost / budget;
+
+            if (ratio <= 0.75) return "Affordable";
+            else if (ratio <= 1.0) return "Not Affordable";
+            else return "Critical";
+        }
+
+        private int ComputePriority(int importance, string urgency)
+        {
+            int urgencyWeight;
+            switch (urgency)
+            {
+                case "High": urgencyWeight = 10; break;
+                case "Medium": urgencyWeight = 5; break;
+                default: urgencyWeight = 2; break; // Low
+            }
+
+            // Importance = 60%, Urgency = 40% of total score
+            int score = (importance * 6) + (urgencyWeight * 4);
+            return Math.Min(score, 100);
+        }
+
+        private string EvaluateRisk(string riskLevel)
+        {
+            switch (riskLevel)
+            {
+                case "Low": return "Safe";
+                case "Medium": return "Moderate";
+                case "High": return "High Risk";
+                default: return "Unknown";
+            }
+        }
+
+        private string GenerateDecision(string affordability, int priorityScore, string riskEvaluation)
+        {
+            // Rule 1: Cost exceeds budget entirely
+            if (affordability == "Critical")
+                return "Not Recommended";
+
+            // Rule 2: High risk blocks immediate action
+            if (riskEvaluation == "High Risk")
+                return "Delay Decision";
+
+            // Rule 3: All green lights
+            if (affordability == "Affordable" && riskEvaluation == "Safe" && priorityScore >= 70)
+                return "Proceed Immediately";
+
+            // Rule 4: Affordable but not peak conditions
+            if (affordability == "Affordable" && priorityScore >= 40)
+                return "Proceed with Caution";
+
+            // Rule 5: Budget is borderline
+            if (affordability == "Not Affordable")
+                return "Delay Decision";
+
+            return "Proceed with Caution";
+        }
+        private string GenerateExplanation(string affordability, int priorityScore,
+                                   string riskEvaluation, string decision)
+        {
+            string a, p, r, c;
+
+            switch (affordability)
+            {
+                case "Affordable":
+                    a = "Budget comfortably covers the cost."; break;
+                case "Not Affordable":
+                    a = "Cost is close to or slightly over budget."; break;
+                default:
+                    a = "Cost greatly exceeds budget — financially dangerous."; break;
+            }
+
+            p = priorityScore >= 70 ? "HIGH priority (Score: " + priorityScore + "/100)." :
+                priorityScore >= 40 ? "MODERATE priority (Score: " + priorityScore + "/100)." :
+                                      "LOW priority (Score: " + priorityScore + "/100).";
+
+            switch (riskEvaluation)
+            {
+                case "Safe": r = "Risk is low."; break;
+                case "Moderate": r = "Moderate risk — proceed with care."; break;
+                default: r = "High risk detected — consider waiting."; break;
+            }
+            switch (decision)
+            {
+                case "Proceed Immediately":
+                    c = "All indicators are favorable."; break;
+                case "Proceed with Caution":
+                    c = "Acceptable, but monitor finances and risk."; break;
+                case "Delay Decision":
+                    c = "Wait until conditions improve."; break;
+                default:
+                    c = "Not advisable at this time."; break;
+            }
+            return a + " " + p + " " + r + " " + c;
+        }
+
+        private void DisplayResult(string affordability, int priorityScore,
+                           string riskEvaluation, string decision, string explanation)
+        {
+            lblAffordabilityResult.Text = affordability;
+            lblPriorityResult.Text = priorityScore + " / 100";
+            lblRiskResult.Text = riskEvaluation;
+            lblDecisionResult.Text = decision;
+            lblExplanationResult.Text = explanation;
+
+            // Color: Affordability
+            switch (affordability)
+            {
+                case "Affordable": lblAffordabilityResult.ForeColor = Color.FromArgb(0, 130, 60); break;
+                case "Not Affordable": lblAffordabilityResult.ForeColor = Color.FromArgb(200, 130, 0); break;
+                case "Critical": lblAffordabilityResult.ForeColor = Color.FromArgb(190, 30, 30); break;
+            }
+            // Color: Risk
+            switch (riskEvaluation)
+            {
+                case "Safe": lblRiskResult.ForeColor = Color.FromArgb(0, 130, 60); break;
+                case "Moderate": lblRiskResult.ForeColor = Color.FromArgb(200, 130, 0); break;
+                case "High Risk": lblRiskResult.ForeColor = Color.FromArgb(190, 30, 30); break;
+            }
+            // Color: Decision
+            switch (decision)
+            {
+                case "Proceed Immediately": lblDecisionResult.ForeColor = Color.FromArgb(0, 130, 60); break;
+                case "Proceed with Caution": lblDecisionResult.ForeColor = Color.FromArgb(200, 130, 0); break;
+                case "Delay Decision": lblDecisionResult.ForeColor = Color.FromArgb(160, 80, 0); break;
+                case "Not Recommended": lblDecisionResult.ForeColor = Color.FromArgb(190, 30, 30); break;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            budtxt.Clear();
+            costtxt.Clear();
+            cmbUrgency.SelectedIndex = -1;
+            Importtxt.Clear();
+            cmbRisk.SelectedIndex = -1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (!ValidateAllInputs()) return;
+
+            double budget = double.Parse(budtxt.Text);
+            double cost = double.Parse(costtxt.Text);
+            int importance = int.Parse(Importtxt.Text);
+            string urgency = cmbUrgency.SelectedItem.ToString();
+            string risk = cmbRisk.SelectedItem.ToString();
+
+            string affordability = CheckAffordability(budget, cost);
+            int priorityScore = ComputePriority(importance, urgency);
+            string riskEvaluation = EvaluateRisk(risk);
+            string decision = GenerateDecision(affordability, priorityScore, riskEvaluation);
+            string explanation = GenerateExplanation(affordability, priorityScore, riskEvaluation, decision);
+
+            DisplayResult(affordability, priorityScore, riskEvaluation, decision, explanation);
+
+        }
+    }
+}
+
